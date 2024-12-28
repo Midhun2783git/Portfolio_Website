ba9ea495fd17077fcfc7e47459611b4e4530bc41
Midhun2783git <99118893+Midhun2783git@users.noreply.github.com>
2024-12-28 12:55:51 +0530

    Update Hero.jsx

--- src/components/components/Hero.jsx
+++ src/components/components/Hero.jsx
@@ -18,7 +18,7 @@ const Hero = () => {
         </div>
 
         <div>
-          <h1 className={`${styles.heroHeadText}text-white`}>Hi, I'm <span className="text-[#915eff]">Midhun</span></h1>
+          <h1 className={`${styles.heroHeadText}text-white`}>Hi, I am <span className="text-[#915eff]">Midhun</span></h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             Description
           </p>
