c78ab36316a6d074319ddde70228c19950a0b956
Midhun2783git <midhunsaminathan@gmail.com>
2024-12-30 14:23:18 +0400

    update

--- src/components/components/About.jsx
+++ src/components/components/About.jsx
@@ -5,11 +5,34 @@ import { styles } from '../../styles';
 import { services } from '../../constants';
 import { fadeIn, textVariant } from '../../utils/motion';
 
-// const ServiceCard = ({ index, title, icon }) => {
-//   return {
-//     <p>{title}</p>
-//   }
-// }
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <img
+          src={icon}
+          alt='web-development'
+          className='w-16 h-16 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
 const About = () => {
   return (
     <>
@@ -22,18 +45,18 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-        I am an aspiring technologist with a strong passion for AI, robotics, and innovative solutions.
+        I'm an aspiring technologist with a strong passion for AI, robotics, and innovative solutions.
         My academic and project experiences have allowed me to work on a variety of challenges,
         from developing simulations to creating interactive applications and exploring machine learning concepts.
         I am skilled in tools such as Python, Blender, and Unreal Engine, which I use to bring my ideas to life.
         Driven by curiosity and a desire to make a meaningful impact, I am always seeking opportunities to learn and contribute to advancements in the tech field.
       </motion.p>
 
-      {/* <div className='mt-20 flex flex-wrap gap-10'>
+      <div className='mt-20 flex flex-wrap gap-10'>
         {services.map((service, index) => (
-          <ServiceCard key={service.title} index={index} {...service}/>
+          <ServiceCard key={service.title} index={index} {...service} />
         ))}
-      </div> */}
+      </div>
     </>
   )
 }
