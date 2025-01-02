b7896d6e8286773b31b9d1ce65c485d806601895
Midhun2783git <midhunsaminathan@gmail.com>
2025-01-02 12:08:13 +0400

    test2

--- src/components/components/Tech.jsx
+++ src/components/components/Tech.jsx
@@ -13,7 +13,7 @@ const Tech = () => {
         <h2 className={`hidden md:block ${styles.sectionHeadText}`}>Technologies</h2>
       </motion.div>
 
-      <div className='flex flex-row flex-wrap justify-center gap-10'>
+      <div className='hidden md:flex flex-row flex-wrap justify-center gap-10'>
         {technologies.map((technology) => (
           <div className='w-28 h-28' key={technology.name}>
             <BallCanvas icon={technology.icon}/>
