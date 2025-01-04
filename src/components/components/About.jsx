51af4b550875cb9dc14033995e445975cece8e09
Midhun2783git <midhunsaminathan@gmail.com>
2025-01-04 13:22:17 +0400

    update

--- src/components/components/About.jsx
+++ src/components/components/About.jsx
@@ -87,23 +87,20 @@ const About = () => {
           className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
         >
           <p>
-            Hi! I'm Midhun Saminathan, a passionate learner and innovator with a
-            strong interest in AI, robotics, and technology-driven solutions.
-            My academic and project experiences have allowed me to explore diverse areas,
-            including simulations, behavior-based robotics, and interactive designs.
-          </p>
+          Hi! I'm Midhun Saminathan, a passionate learner and innovator with a strong
+          interest in AI, robotics, and technology-driven solutions.
+          My academic and project experiences have enabled me to explore diverse areas.
+          I like tackling complex challenges and transforming ideas into impactful
+          outcomes, whether by designing intelligent systems or creating user-focused
+          applications.
           <br />
-          <p>
-            I enjoy tackling complex challenges and transforming ideas into impactful
-            outcomes, whether it’s through designing intelligent systems or developing
-            user-focused applications. With a keen eye for detail and a problem-solving
-            mindset, I strive to create solutions that not only address real-world needs
-            but also push the boundaries of innovation.
-          </p>
           <br />
-          I am driven by curiosity and a commitment to continuous growth, eager to
-          contribute to projects that shape the future of technology and make a
-          meaningful difference.
+          With a keen eye for detail and a problem-solving mindset,
+          I aim to develop solutions that address real-world needs while pushing
+          the boundaries of innovation. Driven by curiosity and a commitment to
+          continuous growth, I am eager to contribute to projects that shape the
+          future of technology and create a meaningful difference.
+          </p>
         </motion.p>
       </div>
 
